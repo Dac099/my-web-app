2a0f8a6cab97b117288679193a97bf7fa9c8942c
Dac099 <davidcejazapata@gmail.com>
2022-10-12 13:10:35 -0500

    bad day

--- index.js
+++ index.js
@@ -1,23 +1,51 @@
 const img = document.getElementById('imgContainer');
 const reloadBtn = document.getElementById('reload');
+const title = document.getElementById('title');
 
-async function getImage(url){
+function buildURL(){
+  const baseURL = 'https://rickandmortyapi.com/api';
+  return `${baseURL}/location/${Math.round(Math.random() * (1 + 126) + 1)}`;
+}
+
+function getCharacters(array){
+  const jsonCharacter = array.map(character => {
+    getCharacterImg(character);
+  });
+
+  return jsonCharacter;
+}
+
+async function getCharacterImg(character){
   try {
-    const random =  Math.round(Math.random() * (0 + 20) + 0);
+    const res = await fetch(character);
+    const data = await res.json();
+
+    console.log(data);
+    return data;
+  } catch (error) {
+    console.log(error);
+  } 
+}
+
+async function getLocation(url){
+  try {
     const response = await fetch(url);
     const data = await response.json();
-    const srcImage = data.results[random].image;
-  
-    img.src = srcImage;
+    const characters = data.residents;
+    const dimension = data.dimension;
+
+    title.innerText = dimension;
+    const residents = getCharacters(characters);
+
+    console.log(residents);
   } catch (error) {
     console.log(error);
     alert('An error ocurred, sorry');
   }
 }
 
-getImage(`https://rickandmortyapi.com/api/character?page=${Math.round(Math.random() * (1 + 42) + 1)}`);
 
 reloadBtn.addEventListener('click', () => {
-  let url = `https://rickandmortyapi.com/api/character?page=${Math.round(Math.random() * (1 + 42) + 1)}`;
-  getImage(url);
+  let url = buildURL();
+  getLocation(url);
 })
