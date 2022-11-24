cd05f8d24a78772584179b48cfb22fbfff3a7f52
Dac099 <davidcejazapata@gmail.com>
2022-11-24 13:30:23 -0600

    creating sections

--- index.js
+++ index.js
@@ -1,6 +1,33 @@
 //Elements for card profile
-const profile_username = document.getElementById('profile_username');
-const profile_email = document.getElementById('profile_email');
-const profile_phone = document.getElementById('profile_phone');
-const profile_website = document.getElementById('profile_website');
-const profile_id = document.getElementById('profile_id');
+const user_name = document.querySelector('.user-name');
+const alias = document.getElementById('profile_username');
+const user_email = document.getElementById('proflie_email');
+const user_phone = document.getElementById('profile_phone');
+const user_website = document.getElementById('profile_website');
+const user_id = document.getElementById('profile_id');
+
+
+function buildUser(iterable){
+  const object_data = {};
+
+  for (const [key, value] of Object.entries(iterable)){
+    object_data[key] = value;
+  }
+
+  return object_data;
+}
+
+// Buil an object from the localstorage data
+const user_data = buildUser(localStorage);
+const {name, username, email, phone, website, id} = user_data;
+
+user_name.innerHTML = name;
+alias.innerHTML = username;
+user_email.innerHTML = email;
+user_phone.innerHTML = phone;
+user_website.innerHTML = website;
+user_id.innerHTML = id;
+
+
+// Print data in the card elements
+
