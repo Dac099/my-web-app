1c6fe2de57e4ddf2f1bbe839248cbe770b5f7c68
Dac099 <davidcejazapata@gmail.com>
2022-12-14 13:00:48 -0600

    albums logic

--- js/albums.js
+++ js/albums.js
@@ -6,11 +6,6 @@ const container = document.getElementById('albums-container');
 const input_album_title = document.getElementById('album-title');
 const album_title_btn = document.getElementById('album-btn');
 
-//example of listeners for editing album title
-input_album_title.addEventListener('pointerleave', () => {
-  console.log(input_album_title.value);
-});
-
 function createAlbumCard(album){
   const album_card = document.createElement('article');
   album_card.dataset.id = album.id;
@@ -52,16 +47,52 @@ function albumCardListeners(album_card, albumId){
   });
 
   preview.addEventListener('click', async () => {
-    //todo: Show a sidebar with all the photos in it
     const photos = await Media.getPhotosByAlbum(photos_url, albumId);
-    console.log(photos);
+    const sidebar = createSidebar(photos, albumId);
+
+    const exist_sidebar = document.querySelector('.sidebar-photos');
+    if(exist_sidebar){
+      exist_sidebar.remove();
+    }
+    
+    console.log(sidebar);
+    container.append(sidebar);
   });
+}
+
+function createSidebar(photos, albumId){
+  const sidebar = document.createElement('aside');
+  sidebar.classList.add('sidebar-photos');
+  sidebar.dataset.albumid = albumId;
+
+  photos.forEach(photo => {
+    const card_photo =  document.createElement('article');
+    const card_title = document.createElement('p');
+    card_title.innerHTML = photo.title;
+    const photo_element = document.createElement('img');
+    photo_element.classList.add('sidebar-photo');
+    const url = photo.url.split('/');
+    url[3] = '200';
+    photo_element.src = url.join('/');
+    
+
+    card_photo.append(card_title);
+    card_photo.append(photo_element);
+    sidebar.append(card_photo);
+  });
+
+  return sidebar;
 }
 
 async function photosAlbumPreview(preview_section, albumId){
   try { 
     const data = await Media.getPhotosByAlbum(photos_url, albumId);
     
+    if(data.length == 0){
+      preview_section.innerHTML = '<p>No photos yet! :c</p>';
+      return;
+    }
+
     //Create preview with just 5 photos
     for(let i = 0; i <= 5; i++){
       const img = document.createElement('img');
@@ -92,8 +123,35 @@ async function appendAlbums(){
   }
 }
 
-//todo: Create a new albul with the album_title_btn
+async function createNewAlbum(){
+  try {
+    let control = 0;
+    album_title_btn.addEventListener('click', async () => {
+      if(input_album_title.value != ''){
+        const album = await Media.addNewAlbum(albums_url, {
+        userId : localStorage.getItem('id'),
+          title: input_album_title.value
+        });
+  
+        const album_card = createAlbumCard(album);
+  
+        container.append(album_card);
+      }else{
+        control++;
+        if(control > 2){
+          alert("Please, write a title!!");
+          control = 0;
+        }else{        
+          alert("What is the album title?")
+        }
+      }
+    });
+  } catch (error) {
+    console.log(error);
+  }
+}
 
 window.onload = () => {
   appendAlbums();
+  createNewAlbum();
 }
